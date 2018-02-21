7c8809342e481ba89dbab95e479333ce0c8e0e16
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-02-21 17:30:41 +0000

    index and form done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+flat = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+flat.save!
+
+flat = Flat.create!(
+  name: 'Brigh & Nice small Flat Paris',
+  address: '12 rue Brochant 75017 Paris',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+flat.save!
+
+flat = Flat.create!(
+  name: 'Big Duplex Amsterdam',
+  address: 'Random Stradt Amsterdam',
+  description: 'Large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+flat.save!
+
+flat = Flat.create!(
+  name: 'Great for party flat Barcelona',
+  address: '12 las Ramblas Barcelna',
+  description: 'Great flat fort parties, bring your friends ! 4 double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 200,
+  number_of_guests: 8
+)
+flat.save!
